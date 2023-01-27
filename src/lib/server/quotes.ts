67ffac430dbffcb408d43b333f17791eeac5be43
Aynh <99479536+Aynh@users.noreply.github.com>
2023-01-27 17:10:04 +0800

    feat: add filtering by (character|quote|show)

--- src/lib/server/quotes.ts
+++ src/lib/server/quotes.ts
@@ -6,16 +6,34 @@ export interface Quote {
 	quote: string;
 }
 
+export type PartialQuote = Partial<Quote>;
+
 export const quotes: Quote[] = data;
 
-export const getQuotes = (count: number): Quote[] => {
+export const getQuotes = (count: number, match: PartialQuote = {}): Quote[] => {
 	const copy = [...quotes];
 
 	const ret: Quote[] = [];
+	const filterQuote = lowercased(match);
 	const max = count < 0 ? quotes.length : Math.min(count, quotes.length);
-	while (ret.length < max) {
-		ret.push(...copy.splice(Math.floor(Math.random() * copy.length), 1));
+	while (ret.length < max && copy.length > 0) {
+		const next = copy.splice(Math.floor(Math.random() * copy.length), 1)[0];
+
+		const low = lowercased(next);
+		if (
+			low.character.includes(filterQuote.character) &&
+			low.quote.includes(filterQuote.quote) &&
+			low.show.includes(filterQuote.show)
+		) {
+			ret.push(next);
+		}
 	}
 
 	return ret;
 };
+
+const lowercased = (quote: PartialQuote): Quote => ({
+	character: (quote.character ?? '').toLowerCase(),
+	quote: (quote.quote ?? '').toLowerCase(),
+	show: (quote.show ?? '').toLowerCase()
+});
--- src/routes/api/[[count=integer]]/+server.ts
+++ src/routes/api/[[count=integer]]/+server.ts
@@ -6,8 +6,15 @@ export const GET = ((request) => {
 	const count =
 		request.params.count !== undefined ? Number.parseInt(request.params.count) : quotes.length;
 
-	const ret = { count, data: getQuotes(count) };
-	return json(ret, {
+	const { searchParams } = request.url;
+	const data = getQuotes(count, {
+		character: searchParams.get('character') ?? undefined,
+		quote: searchParams.get('quote') ?? undefined,
+		show: searchParams.get('show') ?? undefined
+	});
+
+	const resp = { count: data.length, data };
+	return json(resp, {
 		headers: {
 			'Access-Control-Request-Method': 'GET',
 			'Access-Control-Allow-Origin': '*'
