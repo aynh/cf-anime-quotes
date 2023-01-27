9848d5b624c148a80b80d75976cb32f3c00933ab
Aynh <99479536+Aynh@users.noreply.github.com>
2023-01-27 17:07:45 +0800

    chore: `yarn format`

--- src/lib/server/quotes.test.ts
+++ src/lib/server/quotes.test.ts
@@ -1,23 +1,23 @@
-import { describe, expect, it } from 'vitest'
-import { getQuotes, quotes } from './quotes'
+import { describe, expect, it } from 'vitest';
+import { getQuotes, quotes } from './quotes';
 
 describe('getQuotes', () => {
 	it('should return correct number of quotes', () => {
 		for (let i = 0; i <= 1000; i += 1) {
-			const quotes = getQuotes(i)
-			expect(quotes.length).toBe(i)
+			const quotes = getQuotes(i);
+			expect(quotes.length).toBe(i);
 		}
 
-		const allQuotes = getQuotes(-1)
-		expect(allQuotes.length).toBe(quotes.length)
-	})
+		const allQuotes = getQuotes(-1);
+		expect(allQuotes.length).toBe(quotes.length);
+	});
 
 	it('should be randomized each runs', () => {
 		for (let i = 0; i <= 1000; i += 1) {
-			const a = getQuotes(5)
-			const b = getQuotes(5)
+			const a = getQuotes(5);
+			const b = getQuotes(5);
 
-			expect(a).not.toStrictEqual(b)
+			expect(a).not.toStrictEqual(b);
 		}
-	})
-})
+	});
+});
--- src/lib/server/quotes.ts
+++ src/lib/server/quotes.ts
@@ -1,21 +1,21 @@
-import { data } from './quotes.json'
+import { data } from './quotes.json';
 
 export interface Quote {
-	character: string
-	show: string
-	quote: string
+	character: string;
+	show: string;
+	quote: string;
 }
 
-export const quotes: Quote[] = data
+export const quotes: Quote[] = data;
 
 export const getQuotes = (count: number): Quote[] => {
-	const copy = [...quotes]
+	const copy = [...quotes];
 
-	const ret: Quote[] = []
-	const max = count < 0 ? quotes.length : Math.min(count, quotes.length)
+	const ret: Quote[] = [];
+	const max = count < 0 ? quotes.length : Math.min(count, quotes.length);
 	while (ret.length < max) {
-		ret.push(...copy.splice(Math.floor(Math.random() * copy.length), 1))
+		ret.push(...copy.splice(Math.floor(Math.random() * copy.length), 1));
 	}
 
-	return ret
-}
+	return ret;
+};
